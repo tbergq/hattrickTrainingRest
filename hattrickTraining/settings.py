55eddb2bb909f0a8f0989a0a1a574cb4e0f73793
Trond Bergquist <trond.bergquist@sigmagroup.no>
2017-02-21 16:45:25 +0100

    Add cors headers

--- hattrickTraining/settings.py
+++ hattrickTraining/settings.py
@@ -16,7 +16,6 @@ import datetime
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -27,7 +26,7 @@ SECRET_KEY = 'lyg^md@%#_wo#e^n38%53l=w4sm8uw&och4*35!s#wcb^kwgag'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'Account'
 ]
 
@@ -54,10 +54,11 @@ REST_FRAMEWORK = {
 }
 
 JWT_AUTH = {
-'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=24)
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=24)
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -87,7 +88,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hattrickTraining.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -97,7 +97,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -117,7 +116,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -130,7 +128,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
